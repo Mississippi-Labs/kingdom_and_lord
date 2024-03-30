5ae9e24bc1f170d697896dfd04bd0476871f7189
WillQ <zsluedem06@gmail.com>
2024-03-30 09:00:02 +0800

    fix merkel bug

--- scripts/set_prod_merkle.sh
+++ scripts/set_prod_merkle.sh
@@ -5,6 +5,6 @@ pushd $(dirname "$0")/..
 echo "Setting pay address for upgrading"
 export ADMIN_ADDRESS=$(cat ./target/$TARGET_NAME/manifest.json | jq -r '.contracts[] | select(.name == "kingdom_lord::admin::kingdom_lord_admin" ).address')
 sozo execute 0x17acb0793d3bfdf9b8058d6ba25215bed0df3949007d0b7676ad335736e444f \
-   set_config --calldata 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,0,100,0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03,0x2e3aa949c5d014218a1194b9d5c84c7457a027fc34826547b8dfe5b52d72220 \
-   --rpc-url $RPC_URL
+   set_config --calldata 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,0,100,0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03,0x78b9b63bbe5319f132fa94fa01bd90b60739760cb1385b3b3490459e78fd55f \
+   --rpc-url http://localhost:5050
 echo "Setting pay address for upgrading done"
--- scripts/set_testnet_merkle.sh
+++ scripts/set_testnet_merkle.sh
@@ -8,6 +8,6 @@ export ACTIONS_ADDRESS=$(cat ./target/$TARGET_NAME/manifest.json | jq -r '.contr
 echo "Setting pay address for upgrading"
 export ADMIN_ADDRESS=$(cat ./target/$TARGET_NAME/manifest.json | jq -r '.contracts[] | select(.name == "kingdom_lord::admin::kingdom_lord_admin" ).address')
 sozo execute 0x17acb0793d3bfdf9b8058d6ba25215bed0df3949007d0b7676ad335736e444f \
-   set_config --calldata 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,0,100,0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03,0x3dfa622028d6dc122aab8334e917c0d9e3cb856fde37e98d47ac54a0ba30f33 \
-   --rpc-url $RPC_URL
+   set_config --calldata 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,0,100,0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03,0x5835e5b8a0346c2c958cec97c376e6002bd274f5a732dd15ad026eb2c7801f7 \
+   --rpc-url http://localhost:5050
 echo "Setting pay address for upgrading done"
