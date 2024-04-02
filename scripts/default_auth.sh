8fa78c75fa9d61c69f871bc75e651955f532daf1
WillQ <zsluedem06@gmail.com>
2024-04-02 23:34:27 +0800

    update auth models

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -29,7 +29,7 @@ echo actions : $ACTIONS_ADDRESS
 echo "---------------------------------------------------------------------------"
 
 # enable system -> component authorizations
-COMPONENTS=("Barn" "OuterCity" "Warehouse" "CityBuilding" "SpawnStatus" "UnderUpgrading" "CityHall" "BuildingAreaInfo" "Barrack" "Troops" "BarrackUnderTraining" "BarnStorage" "WarehouseStorage")
+COMPONENTS=("SpawnStatus" "Barn" "BarnStorage" "Barrack" "Troops" "BarrackUnderTraining" "BarrackWaitingToTrain" "CityBuilding" "UnderUpgrading" "WaitingToUpgrade" "CityHall" "College" "OuterCity" "Stable" "StableUnderTraining" "StableWaitingToTrain" "BuildingAreaInfo" "Warehouse" "WarehouseStorage")
 
 for component in ${COMPONENTS[@]}; do
     sozo auth grant --world $WORLD_ADDRESS --rpc-url $RPC_URL writer  $component,$ACTIONS_ADDRESS 
