f2e417a080a3e000ed80cfffd9759df194b75da9
WillQ <zsluedem06@gmail.com>
2024-04-12 15:53:34 +0800

    update gen block script

--- scripts/gen_block.sh
+++ scripts/gen_block.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 set +xo pipefail
-
+export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address')
+export ACTIONS_ADDRESS=$(cat ./manifests/dev/manifest.json| jq -r '.contracts[] | select(.name == "kingdom_lord::actions::kingdom_lord_controller" ).address')
 while true; do
-    sozo execute --private-key 0x2bbf4f9fd0bbb2e60b0316c1fe0b76cf7a4d0198bd493ced9b8df2a3a24d68a --account-address 0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca --rpc-url http://localhost:5050 \
-        0x1c9c0eac696e8b998fd586792939e7d902312e08ec45b6795de12e52b723e10 get_resource --calldata 0x2bbf4f9fd0bbb2e60b0316c1fe0b76cf7a4d0198bd493ced9b8df2a3a24d68a
+    sozo execute --world $WORLD_ADDRESS --rpc-url http://localhost:5050 \
+    --private-key 0x2bbf4f9fd0bbb2e60b0316c1fe0b76cf7a4d0198bd493ced9b8df2a3a24d68a --account-address 0xb3ff441a68610b30fd5e2abbf3a1548eb6ba6f3559f2862bf2dc757e5828ca \
+        $ACTIONS_ADDRESS get_resource --calldata 0x2bbf4f9fd0bbb2e60b0316c1fe0b76cf7a4d0198bd493ced9b8df2a3a24d68a
     sleep 1
 done
