d472a8a1bf9cc142e70fe4347c799eaa23098cca
WillQ <zsluedem06@gmail.com>
2024-04-29 00:35:41 +0800

    update test merkel

--- scripts/set_testnet_merkle.sh
+++ scripts/set_testnet_merkle.sh
@@ -8,6 +8,6 @@ export WORLD_ADDRESS=$(cat ./manifests/dev/manifest.json | jq -r '.world.address
 export ADMIN_ADDRESS=$(cat ./manifests/dev/manifest.json| jq -r '.contracts[] | select(.name == "kingdom_lord::admin::kingdom_lord_admin" ).address')
 sozo execute --world $WORLD_ADDRESS --rpc-url http://localhost:5050 \
     $ADMIN_ADDRESS \
-   set_config --calldata 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,0,100,0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03,0x4ce6ddcf8d8a12f0c4e570f056f428fe27002649e9f18b308773d4a66f79651
+   set_config --calldata 0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7,0,100,0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03,0x740b06b9bd4e0fe59421c35bd27b1ab0f5ebb507a4ae9ae6207d19e80df104d
 
 echo "Setting pay address for upgrading done"
