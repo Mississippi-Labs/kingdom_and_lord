aefdf47374a171949ea725f32e66c5ec58d2c626
WillQ <zsluedem06@gmail.com>
2024-05-08 20:21:03 +0800

    fix test script

--- scripts/test/spawn.sh
+++ scripts/test/spawn.sh
@@ -2,4 +2,6 @@ source "$(dirname "$0")/../local_env.sh"
 
 sozo execute --private-key 0xc5b2fcab997346f3ea1c00b002ecf6f382c5f9c9659a3894eb783c5320f912 \
     --account-address 0x29873c310fbefde666dc32a1554fea6bb45eecc84f680f8a2b0a8fbb8cb89af \
+    --world $WORLD_ADDRESS \
+    --rpc-url $RPC_URL \
     $ACTIONS_ADDRESS spawn
